56f6f0038289b804db4764f11461fb39a7a2c04d
snupa <adrian@unloq.io>
2018-01-18 14:28:12 +0200

    Added block option to stop thorin app if eenabled

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 'use strict';
 const initStream = require('./lib/logStream');
 /**
-* The plugin will pipe all thorin.js logs to loglet.io
-* */
+ * The plugin will pipe all thorin.js logs to loglet.io
+ * */
 
-module.exports = function(thorin, opt, pluginName) {
+module.exports = function (thorin, opt, pluginName) {
   opt = thorin.util.extend({
     logger: pluginName || 'loglet',
     id: thorin.id,  // the process id
     enabled: true,
+    block: false, // Should we stop the thorin bootup if we cannot connect/
     boot: true,   // send the boot event?
     ignore: [], // array of loggers to ignore.
     gateway: process.env.LOGLET_GATEWAY || null,
@@ -16,15 +17,16 @@ module.exports = function(thorin, opt, pluginName) {
     secret: process.env.LOGLET_SECRET
   }, opt);
   const streamObj = initStream(thorin, opt);
-  const logObj = {};
+  const logObj = {},
+    logger = thorin.logger('loglet');
   thorin.logger.pipe(streamObj.pipe());
 
   /* Enable/disable the stream */
-  logObj.enable = function() {
+  logObj.enable = function () {
     streamObj.enable();
     return logObj;
   }
-  logObj.disable = function() {
+  logObj.disable = function () {
     streamObj.disable();
     return logObj;
   }
@@ -32,8 +34,14 @@ module.exports = function(thorin, opt, pluginName) {
   /*
   *  Initialize the connection BEFORE we call the run fn.
   *  */
-  logObj.run = function(done) {
-    streamObj.start(done);
+  logObj.run = function (done) {
+    streamObj.start((e) => {
+      if (!e) return done();
+      if (opt.block) return done(e);
+      logger.warn(`Could not connect to loglet.io`);
+      logger.debug(e);
+      done();
+    });
   }
 
   logObj.options = opt;
